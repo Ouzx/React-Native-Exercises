ccf4170f024654e8bfce437afbdaee819afe14b1
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-27 02:32:31 +0300

    Exercise 4.3
    I've learned how to show images

--- rn-starter/src/componets/ImageDetail.js
+++ rn-starter/src/componets/ImageDetail.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = (props) => {
     return (
-        <Text>{props.title}</Text>
+        <View>
+            <Image source={ require('../../assets/beach.jpg')}/>
+            <Text>{props.title}</Text>
+        </View>
     );
 };
 
