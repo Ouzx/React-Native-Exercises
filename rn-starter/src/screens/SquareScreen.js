a867032bfc351dcb47d1ae98a105f9f6713fde9a
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-28 13:00:14 +0300

    Exercise 7.4
    I added limiter for increasing and decreasing adjustment.

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -7,6 +7,23 @@ const SquareScreen = () => {
     const [blue, setBlue] = useState(0);
 
     const COLOR_INCREMENT = 20;
+
+    const setColor = (color, change) => {
+        switch (color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change);
+                return;
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+                return;
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+                return;
+            default:
+                return;
+       }
+        
+    }
     return (
         <View>
             <Text>
@@ -14,18 +31,18 @@ const SquareScreen = () => {
             </Text>
             <ColorAdjuster
                 color='Red'
-                onIncrease={ () => setRed(red + COLOR_INCREMENT)}
-                onDecrease={ () => setRed(red - COLOR_INCREMENT)}
+                onIncrease={ () => setColor(red, COLOR_INCREMENT)}
+                onDecrease={ () => setColor(red, -1 * COLOR_INCREMENT)}
             />
             <ColorAdjuster
                 color='Green'
-                onIncrease={ () => setGreen(green + COLOR_INCREMENT)}
-                onDecrease={ () => setGreen(green - COLOR_INCREMENT)}
+                onIncrease={ () => setColor(green, COLOR_INCREMENT)}
+                   onDecrease={ () => setColor(green, -1 * COLOR_INCREMENT)}
             />
             <ColorAdjuster
                 color='Blue'
-                onIncrease={ () => setBlue(blue + COLOR_INCREMENT)}
-                onDecrease={ () => setBlue(blue - COLOR_INCREMENT)}
+                onIncrease={ () => setColor(blue, COLOR_INCREMENT)}
+                onDecrease={ () => setColor(blue, -1 * COLOR_INCREMENT)}
             />
             <View style={{width: 50, height: 50, backgroundColor: `rgb(${red}, ${green}, ${blue} )`} } />
         </View>
