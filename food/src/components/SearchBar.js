f27be03e786533d3e40f81aa2a21555f7b344c30
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-29 01:43:56 +0300

    10.4
    Added state to SearchBar component

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-const SearchBar = () => {
+
+const SearchBar = ({ term, onTermChanged}) => {
     return (
-        <View style={styles.background}>
-            <Feather name='search' size={ 30 }/>
-            <Text>Search Bar</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather
+                name='search'
+                style={styles.iconStyle}            
+            />
+            <TextInput
+                placeholder='Search'
+                autoCapitalize='none'
+                autoCorrect={ false }
+                value={term}
+                onChangeText={newTerm => onTermChanged(newTerm)}
+                style={styles.inputStyle}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#cfd8dc',
         height: 50,
         borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+        padding: 5,
+    },
+    inputStyle: {
+        // borderColor: 'black',
+        // borderWidth: 1,
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyle: {
+        alignSelf: 'center',
+        fontSize: 35,
         marginHorizontal: 15,
     }
 });
