635c27fae8ea936d4d8768539ef3746a998e9dc7
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-29 01:49:29 +0300

    10.5
    Now I can handle when submit editing finish

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = ({ term, onTermChanged}) => {
+const SearchBar = ({ term, onTermChanged, onTermSubmit}) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather
@@ -14,7 +14,8 @@ const SearchBar = ({ term, onTermChanged}) => {
                 autoCapitalize='none'
                 autoCorrect={ false }
                 value={term}
-                onChangeText={newTerm => onTermChanged(newTerm)}
+                onChangeText={onTermChanged}
+                onEndEditing={onTermSubmit}
                 style={styles.inputStyle}
             />
         </View>
