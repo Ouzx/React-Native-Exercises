93f771a78dd01331c5b912606b2212556a34f8cd
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-29 14:43:39 +0300

    10.4
    Set up of resultShowScreen

--- food/App.js
+++ food/App.js
@@ -1,9 +1,11 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import SearchScreen from './src/screens/SearchScreen'
+import ResultShowScreen from './src/screens/ResultShowScreen';
 
 const navigator = createStackNavigator({
   Search: SearchScreen,
+  Result: ResultShowScreen,
 }, {
   initalRouteName: 'Search',
   defaultNavigationOptions: {
