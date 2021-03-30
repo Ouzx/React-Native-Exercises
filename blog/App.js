a5a00d8afe83f7f479cf27d24e5f589393c36896
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-30 21:00:01 +0300

    10.6
    Created dataContext. This is a reuseable data context creator. Easy to set up and easy to use

--- blog/App.js
+++ blog/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
-import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import IndexScreen from "./src/screens/IndexScreen";
+import { Provider } from "./src/context/BlogContext";
 
-const navigator = createStackNavigator({
-  Index: IndexScreen,
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
   }
-});
+);
 
 const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
-}
+};
--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,23 +1,22 @@
-import React, {useReducer} from 'react';
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
 const blogReducer = (state, action) => {
-    switch (action.type) {
-        case 'add_blogpost':
-            return [...state, { title: `Blog post #${state.length + 1}` }];
-        default:
-            return state;
-    };
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: `Blog post #${state.length + 1}` }];
+    default:
+      return state;
+  }
 };
 
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-    
-    const addBlogPost = () => {
-        dispatch({ type: 'add_blogpost' });
-    };
-
-    return <BlogContext.Provider value={{data: blogPosts, addBlogPost: addBlogPost}}>{children}</BlogContext.Provider>
+const addBlogPost = (dispatch) => {
+  return () => {
+    dispatch({ type: "add_blogpost" });
+  };
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
