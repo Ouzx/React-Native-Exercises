213cac27f6905371906a8790c870a93518aaa739
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-03-30 15:49:41 +0300

    10.5.8
    Example of how send data to child as an object

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const BlogContext = React.createContext();
 
+const blogPosts = [
+    {title: 'Blog Post #1'},
+    {title: 'Blog Post #2'},
+    {title: 'Blog Post #3'},
+    {title: 'Blog Post #4'},
+]
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={135989}>{children}</BlogContext.Provider>
+    return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
 };
 
 export default BlogContext;
