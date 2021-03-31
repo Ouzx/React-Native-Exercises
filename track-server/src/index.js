bfe8213733ddc3d88584ba95ece1d78557cd9dbf
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-04-01 00:34:56 +0300

    10.9.1
    I added mongodb connection and nodemon for auto refresh the server

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://admin:admin@cluster0.nlyd5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
