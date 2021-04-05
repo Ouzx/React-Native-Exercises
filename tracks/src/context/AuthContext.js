ca32976be8ee5651af60dc06a30d7f0913b9f410
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-04-06 00:03:40 +0300

    11.2.5
    Simple /signup request to my express api over axios

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import createDataContext from "./createDataContext";
-
+import trackerApi from "../api/tracker";
 const authReducer = (state, action) => {
   switch (action.type) {
     default:
@@ -8,7 +8,14 @@ const authReducer = (state, action) => {
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {};
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
 };
 const signin = (dispatch) => {
   return ({ email, password }) => {};
