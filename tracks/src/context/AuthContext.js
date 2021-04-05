387f5fa79661476c40e8442b750737da727a2250
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-04-06 00:22:40 +0300

    Now I can store tokens in device

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,28 +1,31 @@
+import AsyncStorage from "@react-native-community/async-storage";
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
 const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errMessage: action.payload };
+    case "signup":
+      return { errMessage: "", token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post("/signup", { email, password });
-      console.log(response.data);
-    } catch (err) {
-      // console.log(err.response.data); // err.message
-      dispatch({
-        type: "add_error",
-        payload: "Something went wron with signup!",
-      });
-    }
-  };
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signup", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signup", payload: response.data.token });
+  } catch (err) {
+    // console.log(err.response.data); // err.message
+    dispatch({
+      type: "add_error",
+      payload: "Something went wron with signup!",
+    });
+  }
 };
+
 const signin = (dispatch) => {
   return ({ email, password }) => {};
 };
@@ -34,5 +37,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signup, signout },
-  { isSignedIn: false, errMessage: "" }
+  { token: null, errMessage: "" }
 );
