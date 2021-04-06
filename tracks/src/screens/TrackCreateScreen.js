a867dcc0a917643bf991b567e91d0766e40b3296
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2021-04-07 00:18:31 +0300

    Location context updated and track name added

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -6,17 +6,20 @@ import useLocation from "../hooks/useLocation";
 import Map from "../components/Map";
 import "../_mockLocations";
 import { Context as LocationContext } from "../context/LocationContext";
-
+import TrackForm from "../components/TrackForm";
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext);
+  const { state, addLocation } = useContext(LocationContext);
 
-  const [err] = useLocation(isFocused, addLocation);
+  const [err] = useLocation(isFocused, (location) => {
+    addLocation(location, state.recording);
+  });
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       <Map />
 
       {err ? <Text>Please grant us location access</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   );
 };
